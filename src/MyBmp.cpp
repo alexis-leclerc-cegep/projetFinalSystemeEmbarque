06e83ac5207fd0a0f64f5f3ceff37528387203aa
= <=>
2022-05-12 21:50:31 -0400

    commentaires

--- src/MyBmp.cpp
+++ src/MyBmp.cpp
@@ -1,22 +1,29 @@
+/*
+ * MyBmp.cpp
+ * 
+ *  Created on: 5/12/2022
+ *      Author: Alexis
+ */
+
 #include "MyBmp.h"
 
 
-
-MyBmp::MyBmp(){
-    //bmp.begin();
-};
+//constructor
+MyBmp::MyBmp(){ };
 
 
+//initialisation
 void MyBmp::init(){
     bmp.begin();
 }
 
-/*
+/* commentee car non-nécessaire
 float MyBmp::getPressure(){
     return bmp.readPressure();
 };
 */
 
+//obtient la température
 float MyBmp::getTemperature(){
     return(bmp.readTemperature());
 };
